4dc7369718a09df0e42b13e44ade2d7f1dbdd9e8
Mithrakalaiselvan <kmithra.kalaiselvan@gmail.com>
2016-11-29 09:51:42 +0530

    Added Api to get Alphabetical list of products

--- app/products/products-routes.js
+++ app/products/products-routes.js
@@ -15,4 +15,40 @@ router.get('/getProduct',function(req,res){
 
     })
 })
+router.get('/getProductList',function(req,res){
+    Product.aggregate([
+
+     {$lookup: {  from: "categories",
+                  localField: "CategoryID",
+                  foreignField: "CategoryID",
+                  as :  "prodcut"
+               }
+     },
+     {
+     $project: {  "ProductID":1,
+                  "ProductName":1,
+                  "SupplierID":1,
+                  "CategoryID":1,
+                  "QuantityPerUnit":1,
+                  "UnitPrice":1,
+                  "Discontinued":1,
+                  "_id":0   
+               }
+    },
+    {
+    $match:    {  "Discontinued":"0"
+               }
+    },
+    {
+    $sort:     {  "ProductName":1
+               }
+    }],function(err,results){
+    if(err){
+        res.send(err);
+    }
+    else{
+        res.send(results);
+    }
+})
+})
 module.exports = router;
